da39d65a65d920c274e0095a902dfdb84b48fe61
Mark Wales <mark@smallhadroncollider.com>
2017-12-07 19:21:02 +0000

    added basic linux setup

---
+++ linux/readme.md
@@ -0,0 +1,25 @@
+# Setting up Linux
+
+## Download and install
+
+- [Chrome](https://www.google.co.uk/chrome/browser/desktop/index.html)
+- [Firefox](https://www.mozilla.org/firefox)
+- [Slack](https://slack.com/) our Slack Organisation is `developme` so [developme.slack.com](https://developme.slack.com/)
+- [Sublime 3](https://www.sublimetext.com/3)
+- [FileZilla](https://filezilla-project.org/download.php?show_all=1)
+- [Vagrant](https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.msi)
+- [Virtualbox](https://www.virtualbox.org/)
+
+## Set up a GitHub account
+
+You can [sign up here](https://github.com/join) (the free plan is fine).
+
+## Installing command line tools
+
+[Click here to download the `setup.sh` file](https://cdn.rawgit.com/develop-me/setup/71857023f186726dc8500c4dfb4c807c955e7985/linux/setup.sh) and save it to your Downloads directory (e.g. `~/Downloads`). Next, in Terminal, run the following:
+
+```
+~/Downloads/setup.sh && source ~/.bashrc && exit
+```
+
+The tab should close itself when it's done. Once that happens you can delete the `setup.sh` file.
---
+++ linux/setup.sh
@@ -0,0 +1,145 @@
+#! /bin/bash
+
+# ===============================================================
+
+vagrant_version="2.0.1"
+php_version="7.0"
+node_version="8.x"
+
+vagrant_boxes=(
+    scotch/box
+    laravel/homestead
+)
+
+vagrant_plugins=(
+    vagrant-vbguest
+    vagrant-hostsupdater
+)
+
+# ===============================================================
+
+clear
+
+printf "\e[35m
+  ____                 _             __  __           
+ |  _ \  _____   _____| | ___  _ __ |  \/  | ___      
+ | | | |/ _ \ \ / / _ \ |/ _ \| '_ \| |\/| |/ _ \     
+ | |_| |  __/\ V /  __/ | (_) | |_) | |  | |  __/     
+ |____/ \___| \_/ \___|_|\___/| .__/|_|  |_|\___|____ 
+                              |_|              |_____|
+
+ \e[32m
+ Linux Setup Script v1.0
+
+ —————————————————————————————————————————————————————
+
+ This will install the following command line tools:
+
+ • Git: version control made simple(ish)
+ • Node: JavaScript on the command line 
+ • Sass: makes CSS betterer 
+ • Composer: installs all the PHP things 
+
+ —————————————————————————————————————————————————————
+
+ Ready? (y/n) \e[39m"
+
+read -r input
+
+if ([ "$input" == "no" ] || [ "$input" == "n" ]); then
+    exit 1
+fi
+
+if [ "$USER" == "root" ]; then
+    printf "
+ \e[34m
+ You are logged in as the root user
+
+ You'll need to create a new user and then run the
+ setup script again
+ 
+ Pick a username (lowercase, no spaces): \e[39m"
+    read -r username
+
+    # create user
+    useradd "$username" -m
+
+    # add to sudo group
+    sudo adduser "$username" sudo
+
+    # set password
+    passwd "$username"
+
+    su -l "$username" 
+
+    exit 0
+fi
+
+printf "\e[34m
+ Ok, let's go...
+ \e[39m
+"
+
+mkdir "$HOME/bin"
+
+# allow non standard repos
+sudo apt-get update -y
+sudo apt-get install -y python-software-properties
+
+cd "$HOME" 
+
+# add latest node repo
+curl -sL "https://deb.nodesource.com/setup_${node_version}" | sudo -E bash -
+
+# add yarn repo
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get install -y git "php${php_version}" "php${php_version}-zip" gcc make ruby ruby-dev nodejs yarn
+sudo gem install sass
+
+# ===============================================================
+
+# composer
+cd "$HOME" 
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    printf "Composer couldn't be installed"
+else
+    php composer-setup.php
+    mv composer.phar "$HOME/bin/composer"
+fi
+
+rm composer-setup.php
+
+# ===============================================================
+
+# vagrant
+
+cd "$HOME"
+wget "https://releases.hashicorp.com/vagrant/${vagrant_version}/vagrant_${vagrant_version}_x86_64.deb"
+sudo dpkg -i "vagrant_${vagrant_version}_x86_64.deb"
+
+printf "\n\nexport PATH=\"$HOME/bin:$HOME/.composer/vendor/bin:$(yarn global bin):$PATH\"" >> "$HOME/.bashrc"
+
+# add vagrant boxes
+for i in "${vagrant_boxes[@]}"
+do
+    vagrant box add "$i" --provider virtualbox
+done
+
+vagrant plugin install "${vagrant_plugins[@]}"
+
+# ===============================================================
+
+printf "\e[35m
+
+ —————————————————————————————————————————————————————
+
+ And we're done!
+ \e[39m
+"
