a9b18416350e113df7daa378fede59276dfaa65f
Mark Wales <mark@smallhadroncollider.com>
2020-01-06 16:09:53 +0000

    fixes composer issue with windows setup script

--- windows/setup.sh
+++ windows/setup.sh
@@ -106,6 +106,8 @@ sudo npm install gulp-cli sass -g
 
 # composer
 cd "$HOME"
+sudo chown "$USER":"$USER" -R "$HOME/.config"
+
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
