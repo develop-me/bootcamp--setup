1225ec7bb81dcf1d8501a684eda3269230ddad0c
Mark Wales <mark@smallhadroncollider.com>
2020-01-08 09:49:31 +0000

    moves zsh message closer to when it's needed

--- linux/setup.sh
+++ linux/setup.sh
@@ -116,14 +116,15 @@ vagrant plugin install "${vagrant_plugins[@]}"
 # ===============================================================
 
 # zsh
+sudo apt-get install -y zsh
+[ -f "$HOME/.zshrc" ] && mv "$HOME/.zshrc" "$HOME/.zshrc.old" # backup old zsh file if it exists
+
 printf "\e[34m
  Installing ZSH
  When it asks if you want to change the shell say yes ('Y')
  \e[39m
 "
 
-sudo apt-get install -y zsh
-[ -f "$HOME/.zshrc" ] && mv "$HOME/.zshrc" "$HOME/.zshrc.old" # backup old zsh file if it exists
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 printf "\nexport PATH=\"$HOME/bin:$HOME/.config/composer/vendor/bin:$PATH\"" >> "$HOME/.zshenv"
 
--- windows/setup.sh
+++ windows/setup.sh
@@ -148,14 +148,15 @@ done
 # ===============================================================
 
 # zsh
+sudo apt-get install -y zsh
+[ -f "$HOME/.zshrc" ] && mv "$HOME/.zshrc" "$HOME/.zshrc.old" # backup old zsh file if it exists
+
 printf "\e[34m
  Installing ZSH
  When it asks if you want to change the shell say yes ('Y')
  \e[39m
 "
 
-sudo apt-get install -y zsh
-[ -f "$HOME/.zshrc" ] && mv "$HOME/.zshrc" "$HOME/.zshrc.old" # backup old zsh file if it exists
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 printf "\nexport PATH=\"\$HOME/bin:\$HOME/.config/composer/vendor/bin:/mnt/c/Program Files/Oracle/VirtualBox/:\$PATH\"" >> "$HOME/.zshenv"
 
